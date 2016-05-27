079cf9c7e5847b51064a79b730ec3f4ccc014543
programmelearn <jonathan-huang@users.noreply.github.com>
2016-05-27 14:58:21 +1000

    Update script.js

--- script.js
+++ script.js
@@ -47,11 +47,9 @@ $(document).ready(function() {
 		}		
 	})
 });
-//tab control
 $(document).ready(function(){
 	$('ul.tabs li').click(function(){
 		var tab_id = $(this).attr('data-tab');
-		
 		$('ul.tabs li').removeClass('current');
 		$('.tab-content').removeClass('current');
 		$(this).addClass('current');
